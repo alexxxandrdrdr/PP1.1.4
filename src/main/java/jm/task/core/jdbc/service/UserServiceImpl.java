5a173f80538ef658244e8b7ad6059799a9cf4e76
Alexandr K <110187179+alexxxandrdrdr@users.noreply.github.com>
2025-05-21 13:13:45 +0300

    UserServiceImpl fixed vol2

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -8,8 +8,6 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
     private UserDao userDao = new UserDaoJDBCImpl();
-    public UserServiceImpl() {
-    }
 
     @Override
     public void createUsersTable() {
